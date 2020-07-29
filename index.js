b6747d627200d466458e05f5032fb048c7b30b7e
Akashdeep <adsk3003@gmail.com>
2020-07-29 16:27:31 +0530

    used api to get latest currency exchange rates & added commands to view exchange rates

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
+const meta = require('./info.json');
 const client = new Discord.Client();
 const fs = require('fs');
+const request = require('request');
+
+const options = {
+    url: 'https://api.exchangeratesapi.io/latest?base=INR',
+    method: 'GET'
+};
+
+request(options, function(err, res, body) {
+    let json = JSON.parse(body);
+    obj = json;
+    json = JSON.stringify(json);
+    
+    fs.writeFile('info.json', json, err => {
+        if (err) {
+            console.log('Error writing file', err)
+        } else {
+            console.log('Successfully wrote file')
+        }
+    });
+    for(i in obj.rates){
+        console.log(i + " : " + obj.rates[i]);
+    }
+});
 
 client.once('ready', () => {
     console.info(`Logged in as ${client.user.tag}!`);
@@ -80,5 +104,81 @@ client.on('message', msg => {
     }
 });
 
+client.on('message', msg => {
+    
+    if(msg.content === '!!crcy'){
+        fs.readFile('info.json',(err, json) => {
+            if (err) throw err;
+            json = json.toString();
+            json = JSON.parse(json);
+            msg.channel.send('Base = INR');
+            for(i in json.rates){
+                console.log(i + " : " + json.rates[i]);
+                msg.channel.send(i + " : " + json.rates[i]);
+            }
+        });
+    }
+
+    var args = msg.content.split(' ');
+    if(args[0] === '!!crcy' && args.length == 2){
+        var base = args[1].toUpperCase();
+        
+        const options = {
+            url: 'https://api.exchangeratesapi.io/latest?base=' + base,
+            method: 'GET'
+        };
+        
+        request(options, function(err, res, body) {
+            let json = JSON.parse(body);
+            obj = json;
+            json = JSON.stringify(json);
+
+            var size = 0;
+            msg.channel.send('Base = ' + base);
+            for(i in obj.rates){
+                console.log(i + " : " + obj.rates[i]);
+                msg.channel.send(i + " : " + obj.rates[i]);
+                size++;
+            }
+            console.log(size);
+            if(size == 0){
+                console.log("Invalid arguments");
+                msg.channel.send("Error : Invalid Arguments");
+            }
+        });
+    }
+    else if(args[0] === '!!crcy' && args.length == 3){
+        var base = args[1].toUpperCase();
+        var symbol = args[2].toUpperCase();
+
+        const options = {
+            url: 'https://api.exchangeratesapi.io/latest?base=' + base + '&symbols=' + symbol,
+            method: 'GET'
+        };
+        
+        request(options, function(err, res, body) {
+            let json = JSON.parse(body);
+            obj = json;
+            json = JSON.stringify(json);
+            // console.log(json);
+            var size = 0;
+            msg.channel.send('Base = ' + base);
+            for(i in obj.rates){
+                console.log(i + " : " + obj.rates[i]);
+                size++;
+                msg.channel.send(i + " : " + obj.rates[i]);
+            }
+            console.log(size);
+            if(size == 0){
+                console.log("Invalid arguments");
+                msg.channel.send("Error : Invalid Arguments");
+            }
+        });
+    }
+    else if(args[0] === '!!crcy' && args.length > 3){
+        console.log("Invalid arguments");
+        msg.channel.send("Error : Invalid Arguments");
+    }
+});
 
 client.login(auth.token);
